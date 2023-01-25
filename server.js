ea5e443ee6826c6d7077c5817d3b978a2cd27d15
Suzana <115670321+SuzBarbosa@users.noreply.github.com>
2023-01-25 20:11:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+import jsonServer from 'json-server';
+
+
 const jsonServer = require(`json-server`);
 const server = jsonServer.create();
-const router = jsonServer.router();
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200;
 
 server.use(router);
 server.use(middlewares);
 
-server.listen(3000, () => {console.log(`JSON Server is running`)});
+server.listen(port);
